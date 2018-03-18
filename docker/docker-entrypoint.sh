6c0599e10858f723613d1b4eb6c27d34dffd27ae
James Oguya <oguyajames@gmail.com>
2018-03-18 23:10:20 +0300

    docker-entrypoint.sh: add command create django superuser

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-sleep 20
-
 psql -h db -U postgres -c "CREATE ROLE onadata WITH LOGIN PASSWORD 'onadata';"
 psql -h db -U postgres -c "CREATE DATABASE onadata OWNER onadata;"
 psql -h db -U postgres onadata -c "CREATE EXTENSION postgis; CREATE EXTENSION postgis_topology;"
@@ -11,4 +9,8 @@ psql -h db -U postgres onadata -c "CREATE EXTENSION postgis; CREATE EXTENSION po
 cd /srv/onadata
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
+
+# create superuser
+python manage.py init_super_user --settings=$DJANGO_SETTINGS_MODULE
+
 python manage.py runserver 0.0.0.0:8000
