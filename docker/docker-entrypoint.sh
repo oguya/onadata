f7696b39f09d0d23f602b58abd5ad89fb05c716c
James Oguya <oguyajames@gmail.com>
2018-03-20 23:48:37 +0300

    django migrations; generate docs

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -4,14 +4,15 @@ psql -h db -U postgres -c "CREATE ROLE onadata WITH LOGIN PASSWORD 'onadata';"
 psql -h db -U postgres -c "CREATE DATABASE onadata OWNER onadata;"
 psql -h db -U postgres onadata -c "CREATE EXTENSION postgis; CREATE EXTENSION postgis_topology;"
 
-. /srv/.virtualenv/bin/activate
+#. /srv/.virtualenv/bin/activate
 
 cd /srv/onadata
-python manage.py migrate --noinput
+python manage.py migrate --noinput --run-syncdb
+cd docs && make html && cd ..
 python manage.py collectstatic --noinput
 
 # create superuser
-#python manage.py createsuperuser --settings=$DJANGO_SETTINGS_MODULE
+#python manage.py createsuperuser
 echo "from django.contrib.auth.models import User; User.objects.filter(email='admin@localhost.local').delete(); User.objects.create_superuser('admin', 'admin@localhost.local', 'admin')" | python manage.py shell
 
 
