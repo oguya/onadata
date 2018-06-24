8e1e6678d07c7ba08d44b5308de4724a210ed2f6
James Oguya <oguyajames@gmail.com>
2018-06-24 15:18:12 +0300

    docker-entrypoint: create django-admin user; install git-cloned reqs.

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-psql -h db -U postgres -c "CREATE ROLE onadata WITH LOGIN PASSWORD 'onadata';"
-psql -h db -U postgres -c "CREATE DATABASE onadata OWNER onadata;"
-psql -h db -U postgres onadata -c "CREATE EXTENSION postgis; CREATE EXTENSION postgis_topology;"
-
-#. /srv/.virtualenv/bin/activate
-
 cd /srv/onadata
+
+# ensure annoying git-cloned reqs. are present
+if [[ ! -d src ]]; then
+    cat requirements/docker-env.pip | grep "^git+" > /tmp/git-requirements.txt
+    pip install -r /tmp/git-requirements.txt
+fi
+
 python manage.py migrate --noinput --run-syncdb
 cd docs && make html && cd ..
 python manage.py collectstatic --noinput
 
-# create superuser
-#python manage.py createsuperuser
-echo "from django.contrib.auth.models import User; User.objects.filter(email='admin@localhost.local').delete(); User.objects.create_superuser('admin', 'admin@localhost.local', 'admin')" | python manage.py shell
-
+python manage.py shell << EOF
+from os import environ
+from django.contrib.auth.models import User
+username=environ.get('DEV_LOGIN_USERNAME')
+password=environ.get('DEV_LOGIN_PASSWORD')
+email=environ.get('DJANGO_ADMIN_EMAIL')
+User.objects.filter(email=email).delete()
+User.objects.create_superuser(username, email, password)
+EOF
 
 python manage.py runserver 0.0.0.0:8000
