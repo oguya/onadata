b42394a0adb8a32f342c07b01eea475a6c2653f7
James Oguya <oguyajames@gmail.com>
2020-10-14 18:43:55 +0300

    minor improvements to django-admin user creation script

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -15,6 +15,7 @@ cd docs && make html && cd ..
 ## create django admin user
 python manage.py shell << EOF
 from os import environ
+import logging
 from django.contrib.auth.models import User
 if environ.get('DEV_LOGIN_USERNAME') and environ.get('DEV_LOGIN_PASSWORD'):
     username = environ.get('DEV_LOGIN_USERNAME')
@@ -22,10 +23,10 @@ if environ.get('DEV_LOGIN_USERNAME') and environ.get('DEV_LOGIN_PASSWORD'):
     email = environ.get('DEV_LOGIN_PASSWORD')
     #User.objects.filter(username=username).delete()
     if not User.objects.filter(username=username).exists():
-        print("user: {} doesn't exist, creating one".format(username))
+        logging.info("user '{}' doesn't exist, creating one".format(username))
         User.objects.create_superuser(username, email, password)
     else:
-        print("user: {} already exists!".format(username))
+        logging.info("user: '{}' already exists!".format(username))
 EOF
 
 python manage.py collectstatic --noinput
